0e757e77a975076672b9cc1d18def07b469c446f
taisiyat <taisiyatk@gmail.com>
2015-04-14 08:03:42 +0300

    ObjC

---
+++ TKAObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TKAObjC
+//
+//  Created by Taisiya on 14.04.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
