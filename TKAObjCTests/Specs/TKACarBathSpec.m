f749530d6a6ec61006d6591f8be1e0fc94357b46
taisiyat <taisiyatk@gmail.com>
2015-05-10 22:17:24 +0300

    kiwi

---
+++ TKAObjCTests/Specs/TKACarBathSpec.m
@@ -0,0 +1,21 @@
+//
+//  TKACarBathSpec.m
+//  TKAAutoreleasePool
+//
+//  Created by Taisiya on 10.05.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+#import "Kiwi.h"
+#import "TKAEnterprise.h"
+
+SPEC_BEGIN(TKACarBathSpec)
+
+describe(@"TKAEnterprise", ^{
+
+});
+
+SPEC_END
